02672922c6d4e90184a51e13ce774a69defa5b05
duysieucutehehe123 <thavananh@gmail.com>
2023-11-26 21:36:53 +0700

    Add project files.

---
+++ QuanLyNetSieuCapVipPro/frmLogin.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace QuanLyNetSieuCapVipPro
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUserName = new TextBox();
+            txtPassword = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            txtUserName.Location = new Point(123, 35);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(357, 33);
+            txtUserName.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(123, 95);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(357, 33);
+            txtPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 25);
+            label1.TabIndex = 2;
+            label1.Text = "Tài khoản";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Mật khẩu";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(486, 35);
+            button1.Name = "button1";
+            button1.Size = new Size(103, 93);
+            button1.TabIndex = 4;
+            button1.Text = "Đăng nhập";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(620, 160);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUserName);
+            Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "frmLogin";
+            Text = "frmLogin";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUserName;
+        private TextBox txtPassword;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+    }
+}
